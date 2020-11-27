496ba9e2fa52d5d21d3f15814619a52e1dde85f9
INDRA\xffiguls <xffiguls@indra.es>
2020-11-27 11:47:51 +0100

    Bugfix

--- create_acc_n_keys.sh
+++ create_acc_n_keys.sh
@@ -1,6 +1,6 @@
-directori_preconf="/volume1/docker/folderclone/git_repo/folderclone";
-directori_intern="config_fc";
-docker run -it --rm -v $directori_preconf/$1_config:/$directori_intern xeviff/fcmanager create accounts $2; 
-docker run -it --rm -v $directori_preconf/$1_config:/$directori_intern xeviff/fcmanager create account-keys $2;
 directori_saspacks=/volume1/docker/folderclone/sas_packs;
-cp -r $directori_saspacks/$1_config $directori_preconf/$1_config;
+directori_intern="config_fc";
+docker run -it --rm -v $directori_saspacks/$1_config:/$directori_intern xeviff/fcmanager create accounts $2; 
+docker run -it --rm -v $directori_saspacks/$1_config:/$directori_intern xeviff/fcmanager create account-keys $2;
+directori_preconf="/volume1/docker/folderclone/git_repo/folderclone";
+cp -r $directori_saspacks/$1_config $directori_preconf;
